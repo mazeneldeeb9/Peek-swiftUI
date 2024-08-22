e0b90cf3734b4cbcab903817fb3f16f500ad292e
mazeneldeeb9 <mazeneldeeb9@gmail.com>
2024-08-22 17:34:12 +0300

    Initial Commit

---
+++ Peek-swiftUI/Peek_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Peek_swiftUIApp.swift
+//  Peek-swiftUI
+//
+//  Created by mazen eldeeb on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Peek_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
