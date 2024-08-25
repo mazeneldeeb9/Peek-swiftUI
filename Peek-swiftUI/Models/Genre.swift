fbc263e8dd2ea5ff2e36dbe0ef2814ccfd59e30d
mazeneldeeb9 <mazeneldeeb9@gmail.com>
2024-08-25 10:23:19 +0300

    add Movie and Genre Models

---
+++ Peek-swiftUI/Models/Genre.swift
@@ -0,0 +1,13 @@
+//
+//  Genre.swift
+//  Peek-swiftUI
+//
+//  Created by mazen eldeeb on 25/08/2024.
+//
+
+import Foundation
+
+struct Genre: Decodable, Identifiable {
+    let id: Int
+    let name: String
+}
