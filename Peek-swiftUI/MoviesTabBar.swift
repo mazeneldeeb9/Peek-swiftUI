0f5d743d831d12240e44b1b9a2951d171fae9308
mazeneldeeb9 <mazeneldeeb9@gmail.com>
2024-08-26 11:20:43 +0300

    - refactor tab bar code - remove content view

--- Peek-swiftUI/MoviesTabBar.swift
+++ Peek-swiftUI/MoviesTabBar.swift
@@ -10,9 +10,7 @@ import SwiftUI
 struct MoviesTabBar: View {
     @AppStorage("selectedTab") private var selectedTab = 0
     
-    init() {
-        UITabBar.appearance().backgroundColor = .mainPurple
-    }
+    
     var body: some View {
         TabView(selection: $selectedTab) {
             MainView()
@@ -21,11 +19,12 @@ struct MoviesTabBar: View {
             FavoriteView()
                 .tabItem { Label("Favourite", systemImage: "heart.fill") }
                 .tag(1)
-        }.toolbarBackground(.mainPurple, for: .tabBar)
-            .tint(.brightPurple)
-        
-            .onAppear{           UITabBar.appearance().unselectedItemTintColor =   .inactiveGrey
-            }
+        }
+        .tint(.brightPurple)
+        .onAppear{
+            UITabBar.appearance().backgroundColor = .mainPurple
+            UITabBar.appearance().unselectedItemTintColor = .inactiveGrey
+        }
     }
 }
 
