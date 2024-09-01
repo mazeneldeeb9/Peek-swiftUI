72c7702066e35a5ace014732aed7333bc5238853
mazeneldeeb9 <mazeneldeeb9@gmail.com>
2024-09-01 11:58:13 +0300

    modify scroll view in moviesdetails view

--- Peek-swiftUI/Modules/MoviesDetailsView/Views/MoviesDetailsView.swift
+++ Peek-swiftUI/Modules/MoviesDetailsView/Views/MoviesDetailsView.swift
@@ -22,48 +22,53 @@ struct MoviesDetailsView: View {
             ErrorView(callAgain: {handler.fetchMovieDetails(for: movieId)},
                       errorMessage: handler.errorMessage ?? "Something went Wrong")
         } else {
-            ScrollView {
-                VStack(alignment: .leading) {
-                    MovieTopImage(urlString: "https://image.tmdb.org/t/p/w500/\(handler.movie.posterPath ?? "")")
-                    HStack {
-                        Text(handler.movie.title ?? "N/A")
+            VStack(alignment: .leading) {
+                MovieTopImage(urlString: "https://image.tmdb.org/t/p/w500/\(handler.movie.posterPath ?? "")")
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        HStack {
+                            Text(handler.movie.title ?? "N/A")
+                                .foregroundStyle(.white)
+                                .font(.title2)
+                                .bold()
+                            Spacer()
+                            Image(systemName: "hand.thumbsup.fill")
+                                .foregroundStyle(.lightYellow)
+                                .bold()
+                            Text(handler.movie.getVoteAverage())
+                                .foregroundStyle(.lightYellow)
+                                .bold()
+                            Text(handler.movie.getVoteCount())
+                                .foregroundStyle(.gray)
+                        }.padding([.horizontal, .top])
+                        HStack {
+                            MovieChip(text: handler.movie.getReleaseYear())
+                            MovieChip(text: handler.movie.getGenres().firstCategory)
+                            MovieChip(text: handler.movie.getGenres().secondCategory)
+                            Spacer()
+                            Image(systemName: "clock.fill").foregroundStyle(.gray)
+                            Text(handler.movie.getDuration())
+                                .foregroundStyle(.gray)
+                        }.padding(.horizontal)
+                        Text("About film")
                             .foregroundStyle(.white)
-                            .font(.title2)
+                            .font(.title3)
                             .bold()
-                        Spacer()
-                        Image(systemName: "hand.thumbsup.fill")
-                            .foregroundStyle(.lightYellow)
-                            .bold()
-                        Text(handler.movie.getVoteAverage())
-                            .foregroundStyle(.lightYellow)
-                            .bold()
-                        Text(handler.movie.getVoteCount())
-                            .foregroundStyle(.gray)
-                    }.padding([.horizontal, .top])
-                    HStack {
-                        MovieChip(text: handler.movie.getReleaseYear())
-                        MovieChip(text: handler.movie.getGenres().firstCategory)
-                        MovieChip(text: handler.movie.getGenres().secondCategory)
-                        Spacer()
-                        Image(systemName: "clock.fill").foregroundStyle(.gray)
-                        Text(handler.movie.getDuration())
-                            .foregroundStyle(.gray)
-                    }.padding(.horizontal)
-                    Text("About film")
-                        .foregroundStyle(.white)
-                        .font(.title3)
-                        .bold()
-                        .padding()
+                            .padding()
                         Text(handler.movie.overview ?? "N/A")
                             .font(.body)
                             .foregroundStyle(.gray)
                             .padding(.horizontal)
                             .lineSpacing(0)
-                    Spacer()
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(.mainPurple)
+                        Spacer()
+                    }
+                    }
+                    
+                
             }
+            .background(.mainPurple)
+            .edgesIgnoringSafeArea(.top)
+            
         }
         
     }
